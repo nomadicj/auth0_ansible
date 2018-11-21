6199b49e889c08de7bfb47aceb302304806ed6c0
nomadicj <j@armstro.ca>
2018-11-21 16:47:44 +0000

    JNA; Updating Get functionality for exception coverage with a few additional tidy ups

--- modules/auth0_client.py
+++ modules/auth0_client.py
@@ -102,7 +102,6 @@ from ansible.module_utils.basic import AnsibleModule
 from auth0.v3.authentication import GetToken
 from auth0.v3.management import Auth0 as a0
 from auth0.v3.exceptions import Auth0Error
-import tenacity
 import json
 import os
 from deepdiff import DeepDiff
@@ -119,49 +118,107 @@ class Auth0(object):
             self.clientsecret, 'https://{}/api/v2/'.format(self.domain))
         return a0(self.domain, token['access_token'])
 
-    @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_delay(10), retry=tenacity.retry_if_exception_type(Auth0Error))
     def get_clients(self):
         return self.auth0.clients.all()
 
-    @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_delay(10), retry=tenacity.retry_if_exception_type(Auth0Error))
     def get_client(self, client_id):
         return self.auth0.clients.get(client_id)
 
-    @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_delay(10), retry=tenacity.retry_if_exception_type(Auth0Error))
     def create_client(self, body):
         return self.auth0.clients.create(body)
 
-    @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_delay(10), retry=tenacity.retry_if_exception_type(Auth0Error))
     def update_client(self, client_id, body):
         return self.auth0.clients.update(client_id, body)
 
-    @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_delay(10), retry=tenacity.retry_if_exception_type(Auth0Error))
     def delete_client(self, client_id):
         return self.auth0.clients.delete(client_id)
 
-    @tenacity.retry(wait=tenacity.wait_fixed(5), stop=tenacity.stop_after_delay(10), retry=tenacity.retry_if_exception_type(Auth0Error))
     def get_clientid(self, name):
         for client in self.get_clients():
             if client['name'] == name:
                 return client['client_id']
         return False
 
+def check_json_diff():
+    # Build new JSON
+    try:
+        with open(input_file) as json_data:
+            import_json = json.load(json_data)
+
+        with open(content_file, "r") as contentfile:
+            content_data = contentfile.read()
+
+        if s3_url and s3_url_placeholder:
+            content_data = content_data.replace(s3_url_placeholder, s3_url)
+        else:
+            result['message'].append("No s3_url was available. No attempt at interpolation attempted.")
+
+        if custom_domain and custom_domain_placeholder:
+            content_data = content_data.replace(custom_domain_placeholder, custom_domain)
+        else:
+            result['message'].append("No Custom Domain was available. No attempt at interpolation attempted.")
+
+        import_json.update(custom_login_page = content_data)
+
+        if app_url and app_url_placeholder:
+            json_string = json.dumps(import_json).replace(app_url_placeholder, app_url)
+            import_json = json.loads(json_string)
+        else:
+            result['message'].append("No app_url was provided. No attempt at interpolation attempted.")
+    except Exception as e:
+        result['message'].append("{} thrown. No assertion possible.".format(e))
+        result['skipped'] = True
+        module.exit_json(**result)
+
+    #  Get existing JSON
+    try:
+        client_id = auth0.get_clientid(name)
+        existing_json = auth0.get_client(client_id)
+        content_hash_change = False
+    except Exception as e:
+        result['message'].append("{} thrown. No assertion possible.".format(e))
+        result['skipped'] = True
+        module.exit_json(**result)
+
+    try:
+        new_content_hash = hashlib.sha256(content_data.encode('utf-8')).hexdigest()
+        old_content_hash = hashlib.sha256(existing_json.encode('utf-8')).hexdigest()
+
+        result['message'].append("Old custom_login_page hash; [{}]".format(old_content_hash))
+        result['message'].append("New custom_login_page hash; [{}]".format(new_content_hash))
+
+        existing_json.get('description', False)
+        old_content_hash = old_content_hash.split(":")[-1]
+
+        if new_content_hash != old_content_hash:
+            result['message'].append("[{}] file change detected.".format("custom_login_page"))
+            # content_hash_change = True
+            # import_json.update(description="custom_login_page:{}".format(new_content_hash))
+    except Exception as e:
+        result['message'].append("Failed to assess new vs old content files.")
+
+    json_diff = DeepDiff(existing_json, import_json, ignore_order=True)
+    except Exception as e:
+        result['message'].append("{} thrown. No assertion possible.".format(e))
+        result['skipped'] = True
+        module.exit_json(**result)
+
 def run_module():
     module_args = dict(
-        domain              = dict(required=True, type='str'),
-        clientid            = dict(required=True, type='str'),
-        clientsecret        = dict(required=True, type='str'),
-        name                = dict(type='str'),
-        state               = dict(default='present', choices=['present','absent'], type='str'),
-        input_file          = dict(type='path'),
-        mode                = dict(default='get', choices=['get','assert','get_all'], type='str'),
-        output_file         = dict(type='path'),
-        app_url             = dict(type='str'),
-        app_url_placeholder = dict(type='str'),
-        s3_url              = dict(type='str'),
-        s3_url_placeholder  = dict(type='str'),
-        content_file        = dict(type='str'),
-        custom_domain       = dict(type='str'),
+        domain                  = dict(required=True, type='str'),
+        clientid                = dict(required=True, type='str'),
+        clientsecret            = dict(required=True, type='str'),
+        name                    = dict(type='str'),
+        state                   = dict(default='present', choices=['present','absent'], type='str'),
+        input_file              = dict(type='path'),
+        mode                    = dict(default='get', choices=['get','assert','get_all'], type='str'),
+        output_file             = dict(type='path'),
+        app_url                 = dict(type='str'),
+        app_url_placeholder     = dict(type='str'),
+        s3_url                  = dict(type='str'),
+        s3_url_placeholder      = dict(type='str'),
+        content_file            = dict(type='str'),
+        custom_domain           = dict(type='str'),
         custom_domain_placeholder = dict(type='str')
     )
 
@@ -179,20 +236,20 @@ def run_module():
         supports_check_mode = True,
     )
 
-    domain          = module.params.get('domain')
-    clientid        = module.params.get('clientid')
-    clientsecret    = module.params.get('clientsecret')
-    name            = module.params.get('name')
-    input_file      = module.params.get('input_file')
-    mode            = module.params.get('mode')
-    output_file     = module.params.get('output_file')
-    app_url         = module.params.get('app_url')
-    app_url_placeholder = module.params.get('app_url_placeholder')
-    content_file    = module.params.get('content_file')
-    s3_url          = module.params.get('s3_url')
-    s3_url_placeholder = module.params.get('s3_url_placeholder')
-    custom_domain   = module.params.get('custom_domain')
-    custom_domain_placeholder = module.params.get('custom_domain_placeholder')
+    domain                      = module.params.get('domain')
+    clientid                    = module.params.get('clientid')
+    clientsecret                = module.params.get('clientsecret')
+    name                        = module.params.get('name')
+    input_file                  = module.params.get('input_file')
+    mode                        = module.params.get('mode')
+    output_file                 = module.params.get('output_file')
+    app_url                     = module.params.get('app_url')
+    app_url_placeholder         = module.params.get('app_url_placeholder')
+    content_file                = module.params.get('content_file')
+    s3_url                      = module.params.get('s3_url')
+    s3_url_placeholder          = module.params.get('s3_url_placeholder')
+    custom_domain               = module.params.get('custom_domain')
+    custom_domain_placeholder   = module.params.get('custom_domain_placeholder')
 
 
     if module.check_mode:
@@ -207,18 +264,34 @@ def run_module():
         module.exit_json(**result)
 
     if mode == "get":
-        client_data = auth0.get_client(auth0.get_clientid(name))
-        for key in ('signing_keys', 'client_secret'):
-            client_data.pop(key, None)
-        client_json = json.dumps(client_data, indent=2)
-        os.makedirs(os.path.dirname(output_file), exist_ok=True)
-        with open(output_file, "w") as jsonfile:
-            jsonfile.write(client_json)
+        try:
+            client_id = auth0.get_clientid(name)
+            if not client_id:
+                raise ValueError('No client returned when [{}] passed.'.format(name))
+
+            client_data = auth0.get_client(client_id)
+
+            for key in ('signing_keys', 'client_secret'):
+                client_data.pop(key, None)
+
+            client_json = json.dumps(client_data, indent=2)
+            if output_file:
+                os.makedirs(os.path.dirname(output_file), exist_ok=True)
+                with open(output_file, "w") as jsonfile:
+                    jsonfile.write(client_json)
+                result['results'].append("Client JSON written to [{}]".format(output_file))
+            else:
+                result['results'].append(client_data)
+        except Exception as e:
+            result['message'].append("{} No get possible.".format(e))
 
     if mode == "get_all":
         client_list = auth0.get_clients()
         for client in client_list:
             result['results'].append('{}:{}'.format(client['name'], client['client_id']))
+
+    if mode == "check":
+        json_diff = check_json_diff()
 
     if mode == "assert":
         try:
